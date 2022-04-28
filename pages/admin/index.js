03082e4a574c1e9f23845cb5a3f1c68670766ef2
warren yun <wyun2006@gmail.com>
2022-04-28 17:04:39 -0400

    create admin pages

---
+++ pages/admin/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Admin() {
+  return (
+    <>
+      <Head>
+        <title>Admin</title>
+      </Head>
+
+        <div className="flex flex-col items-center justify-center h-screen text-center">
+            <h1 className="text-3xl text-green-400">Your Assignments</h1>
+            <div>
+                
+            </div>
+        </div>
+
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Link from "next/link";
 
-import { FaGoogle } from 'react-icons/fa';
+import { FaGoogle } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen text-center'>
-      <h1 className='mb-8 text-2xl'>Mr Chen's FRQ Submission Page</h1>
-      <button className='px-3 py-4 border border-pink-600 rounded-md'>
-        <FaGoogle size={25} className='inline mr-2' /> Sign in with Google
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="mb-6 text-2xl">Mr Chen's FRQ Submission Page</h1>
+      <button className="px-3 py-4 mb-4 border border-pink-600 rounded-md">
+        <FaGoogle size={25} className="inline mr-2" /> Sign in with Google
       </button>
+      <Link href="/admin">
+        <h1 className="duration-200 hover:cursor-pointer hover:text-cyan-400">
+          (If you are Mr. Chen, just click here!)
+        </h1>
+      </Link>
     </div>
-  )
+  );
 }
