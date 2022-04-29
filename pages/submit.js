103c0cd35b7b04cbde75f1cb7d478c3d95edef49
warren yun <wyun2006@gmail.com>
2022-04-29 01:36:24 -0400

    submit page lookin kinda fresh (not really)

--- pages/submit.js
+++ pages/submit.js
@@ -6,9 +6,11 @@ export default function Submit() {
     firestoreApp.collection("assignments"),
     {}
   );
+  
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-start justify-start h-full p-6">
+      <h1 className="mb-4 text-3xl">Submit</h1>
       <form>
         <div>
           <label for="assignment">Choose an assignment: </label>
@@ -19,7 +21,18 @@ export default function Submit() {
                 ))
               : null}
           </select>
+          <h1 className="mb-4 text-2xl">Paste your code here: </h1>
+          <textarea
+            name="code"
+            id="code"
+            cols="150"
+            rows="30"
+            className="p-6 mb-2 rounded-2xl"
+          />
         </div>
+        <button className="px-4 py-3 text-2xl duration-200 border border-green-400 rounded-lg hover:bg-green-400">
+          Submit
+        </button>
       </form>
     </div>
   );
