a43e2bf7fd745c6addcd7e1cebf3206e1420d851
warren yun <wyun2006@gmail.com>
2022-04-29 14:07:21 -0400

    init basic navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,18 +1,24 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { FaHome } from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
 
 export default function Nav() {
-    const routes = [
-        {
-            "route": "/",
-            "icon": <FaHome size={30} />
-        }
-    ]
-    
-    return (
-        <div className="flex items-center justify-center w-screen">
-            
-        </div>
-    );
+  const routes = [
+    {
+      route: "/",
+      icon: <FaHome size={35} />,
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-end w-screen px-5 py-3 bg-black">
+      {routes.map((route, i) => (
+        <Link href={route.route} passHref>
+          <div className="text-white duration-200 hover:cursor-pointer hover:text-green-400">
+            {route.icon}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import AuthProvider from "../components/contexts/AuthContext";
+import Nav from "../components/Nav";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Nav />
       <div className="text-white bg-[#1f1f1f]">
         <Component {...pageProps} />
       </div>
