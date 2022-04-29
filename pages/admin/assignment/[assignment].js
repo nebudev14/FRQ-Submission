bdb21beb4b4fa0287566b6bb8d8e481a75d0fa38
warren yun <wyun2006@gmail.com>
2022-04-29 09:49:59 -0400

    IT RENDERS

--- pages/admin/assignment/[assignment].js
+++ pages/admin/assignment/[assignment].js
@@ -1,24 +1,26 @@
-import { useDocument } from "react-firebase-hooks/firestore";
-import { doc, query, collection } from "firebase/firestore";
+import { firestoreApp } from "../../../firebase";
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 export default function Assignment(props) {
-    
+    const [value, loading, error] = useCollection(
+        firestoreApp.collection('responses'),
+        {
+          snapshotListenOptions: { includeMetadataChanges: true },
+        }
+    )
 
+    console.log(value)
+    
     return (
         <div className="h-screen p-6 ">
             <h1 className="text-3xl">{props.assignment} submissions</h1>
             <div className="flex flex-col items-center justify-start">
-                
+                {value && value.docs.map((doc, i) => (
+                    <div key={i}>
+                        <h1>{doc.data().email}</h1>
+                    </div>
+                ))}
             </div>
         </div>
     );
-}
-
-export const getServerSideProps = async context => {
-    const {assignment} = context.params;
-    return {
-        props: {
-            assignment
-        }
-    }
 }
