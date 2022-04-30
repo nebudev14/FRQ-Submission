dfb51332ac407a9d1969bf427a7bc5e95573cbe1
warren yun <wyun2006@gmail.com>
2022-04-29 23:04:15 -0400

    thank you dynamic navbar very cool

--- components/Nav.js
+++ components/Nav.js
@@ -2,14 +2,27 @@ import Link from "next/link";
 
 import { FaHome } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
+import { MdAssignment } from "react-icons/md";
 import { useAuth } from "../components/contexts/AuthContext";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { collection, query, where } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { firestoreApp } from "../firebase";
 
 export default function Nav() {
   const { currentUser, logout } = useAuth();
   const [error, setError] = useState("");
+  const [admin, setAdmin] = useState(false);
   const router = useRouter();
+
+  const [user, loading, userError] = useCollectionData(
+    query(collection(firestoreApp, "users"), where("admin", "==", true))
+  );
+
+  useEffect(() => {
+    if (user && currentUser && user[0].email === currentUser.email) setAdmin(true);
+  }, [user, setAdmin, currentUser]);
 
   const handleLogout = async () => {
     setError("");
@@ -33,13 +46,25 @@ export default function Nav() {
     <div className="flex items-center justify-start w-full px-5 py-3 bg-black">
       {routes.map((route, i) => (
         <Link href={route.route} key={i} passHref>
-          <div className="text-white duration-200 hover:cursor-pointer hover:text-green-400">
+          <div className="mr-auto text-white duration-200 hover:cursor-pointer hover:text-green-400">
             {route.icon}
           </div>
         </Link>
       ))}
+      {admin && currentUser != null ? (
+        <Link href="/admin" passHref>
+          <div
+            className="mr-4 text-white duration-200 hover:cursor-pointer hover:text-green-400"
+          >
+            <MdAssignment size={30} />
+          </div>
+        </Link>
+      ) : null}
       {currentUser != null ? (
-        <div className="ml-auto text-white duration-200 hover:cursor-pointer hover:text-red-500" onClick={handleLogout}>
+        <div
+          className="text-white duration-200 hover:cursor-pointer hover:text-red-500"
+          onClick={handleLogout}
+        >
           <BiLogOut size={30} />
         </div>
       ) : null}
